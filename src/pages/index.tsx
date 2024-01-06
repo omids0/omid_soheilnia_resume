eb2a1ad340e90cfc811b0cccbd872213ea3cc07c
Omid <soheilnia.o@entekhabelectronic.com>
2024-01-06 11:30:03 +0330

    layout set

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>salam</div>;
+  return (
+    <div className="flex items-center">
+      <div className="min-w-[60rem] mx-auto min-h-[100vh] flex flex-row-reverse">
+        <div className="min-w-[15rem] bg-cyan-100 p-4 rounded-b-[1rem] rtl">
+          1
+        </div>
+        <div className="w-full p-4 rtl">2</div>
+      </div>
+    </div>
+  );
 }
