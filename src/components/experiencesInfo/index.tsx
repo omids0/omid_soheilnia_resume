934363c367a73ee3a12547dd120269c89aec4b35
Omid <soheilnia.o@entekhabelectronic.com>
2024-01-23 11:36:55 +0330

    experiences data added

--- src/components/experiencesInfo/index.tsx
+++ src/components/experiencesInfo/index.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react";
 import ExperiencesCard from "./experiencesCard";
+import { experiencesInfo } from "@/data/experiencesInfo";
 
 const ExperiencesInfo: FC = () => {
   return (
     <div>
       <p className="font-bold text-base mb-4">تجربیات</p>
       <div className="flex flex-col gap-2">
-        <ExperiencesCard />
-        <ExperiencesCard />
-        <ExperiencesCard />
-        <ExperiencesCard />
+        {experiencesInfo.fa
+          .map((item) => <ExperiencesCard key={item.name} data={item} />)
+          .reverse()}
       </div>
     </div>
   );
