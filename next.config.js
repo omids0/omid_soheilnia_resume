fc8568f1effa629b27f12a7e30220aaf2e9a2310
Omid <soheilnia.o@entekhabelectronic.com>
2024-02-24 12:40:32 +0330

    add loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  //reactStrictMode: true,
-}
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/nextjs-pages",
+  assetPrefix: "/nextjs-pages",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
