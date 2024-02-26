65f24be015e37e2d0afca2f1e04ce56329cbdc63
Omid <soheilnia.o@entekhabelectronic.com>
2024-02-26 16:15:57 +0330

    ExperiencesCard done

--- src/components/experiencesInfo/experiencesCard.tsx
+++ src/components/experiencesInfo/experiencesCard.tsx
@@ -1,4 +1,5 @@
 import type { CompanyInfo } from "@/data/experiencesInfo";
+import moment from "moment";
 import Image from "next/image";
 import { type FC } from "react";
 
@@ -6,11 +7,19 @@ type Props = {
   data: CompanyInfo;
 };
 
-// TODO start time and end time
-
 const ExperiencesCard: FC<Props> = ({ data }) => {
+  const startDate = moment(data.start.join("-"));
+  const endDate = moment(data.end?.join("-"));
+
+  const days = endDate.diff(startDate, "days");
+  const years = Math.floor(days / 365);
+  const months = Math.floor((days % 365) / 30);
+
+  const startMonth = moment(data.start[1], "M").format("MMM");
+  const endMonth = data.end && moment(data.end[1], "M").format("MMM");
+
   return (
-    <div className="bg-gray-50 p-4 rounded-lg flex gap-2">
+    <div className="bg-gray-50 p-4 rounded-lg flex gap-2 max-w-[41rem]">
       <Image
         src={data.logo}
         alt="companyIcon"
@@ -23,22 +32,30 @@ const ExperiencesCard: FC<Props> = ({ data }) => {
         }}
       />
       <div>
-        <div className="flex justify-between min-w-[40rem]">
+        <div className="flex justify-between w-[36rem]">
           <div className="flex flex-col">
             <p className="font-bold text-gray-900">{data.myPosition}</p>
             <p className="text-sm text-gray-800">{`${data.name} (${data.scope}) - ${data.employmentType}`}</p>
           </div>
-          <div className="flex gap-2 text-gray-500">
-            <p className="text-sm text-gray-400">
+          <div className="flex-col text-gray-500">
+            <p className="text-sm text-gray-400 ltr">
               {`${data.city}, ${data.country} . ${data.locationType}`}
             </p>
-            {"/"}
-            <p className="text-sm text-gray-400">{`Jun - Mar - 5Mon`}</p>
+            <div className="flex ltr text-sm text-gray-400 gap-1">
+              <p>{`${startMonth} ${data.start[0]} -`}</p>
+              <p>{`${
+                data.end ? `${endMonth} ${data.end[0]}` : "Present"
+              } -`}</p>
+              <p className="flex">
+                {years !== 0 && `${years}yr,`}
+                {months !== 0 && `${months}mo`}
+              </p>
+            </div>
           </div>
         </div>
         <div className="mt-1">
           <p className="font-bold text-xs text-gray-700">Worked On:</p>
-          <div className="flex gap-2 flex-wrap max-w-[40rem] mt-1">
+          <div className="flex gap-2 flex-wrap max-w-[36rem] mt-1">
             {data.skills.map((item, index) => (
               <p key={item} className="text-xs text-gray-600">
                 {index > 0 && "-"}
