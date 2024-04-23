2a68a12e0f966de2447a706af25cd58f52667c03
Omid <soheilnia.o@entekhabelectronic.com>
2024-04-23 16:22:16 +0330

    meta removed from document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-import { Metadata } from "next";
-// ... other imports
-
-export const metadata: Metadata = {
-  title: "Omid SoheilNia Resume",
-  description: "This is Omid SoheilNia CV",
-  // ... other options
-};
-
 export default function Document() {
   return (
     <Html lang="en">
