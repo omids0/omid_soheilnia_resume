39c8624f33c87c873ec1c43d27bdb72ba6d4587f
Omid <soheilnia.o@entekhabelectronic.com>
2024-04-23 16:11:18 +0330

    meta data added to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { Metadata } from "next";
+// ... other imports
+
+export const metadata: Metadata = {
+  title: "Omid SoheilNia Resume",
+  description: "This is Omid SoheilNia CV",
+  // ... other options
+};
+
 export default function Document() {
   return (
     <Html lang="en">
